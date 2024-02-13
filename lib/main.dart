6b1961cf13103d5fb2a24684bd50ecabcb51b218
Raj Aryan <raj.a@harting.com>
2024-02-13 15:48:56 +0530

    project-setup

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,6 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold();
   }
 }
